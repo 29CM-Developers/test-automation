4a8fc3df1927f3c7fcfdfbeff51e7c08cd51e2f4
dajeong <dajjeong@29cm.co.kr>
2023-07-03 14:45:08 +0900

    iOS Login Test Sample Code

--- ios_automation/test_cases/sample_test.py
+++ ios_automation/test_cases/sample_test.py
@@ -4,35 +4,31 @@ import traceback
 
 from time import sleep
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
 
 class AutomationTesting:
+
+
     def default_test(self, wd, error_text=''):
+
+        id_29cm = 'dajjeong@29cm.co.kr'
+        password_29cm = '29CMdajjeong!'
+
         try:
-            # 요소 찾기 & 사용
-            wd.find_element(AppiumBy.ID, '').click()
-            wd.find_element(AppiumBy.XPATH, '').text
-            wd.find_element(AppiumBy.ID, '').send_keys('')
-            wd.find_element(AppiumBy.ID, '').screenshot('filename.png')
-            wd.find_element(AppiumBy.IOS_PREDICATE, '{{종류}} == XCUIElementTypeButton')
-            # 앱 실행
-            wd.execute_script('mobile:activateApp', {'bundleId': '{{bundle id}}'})
-            # 앱 종료
-            wd.terminate_app('{{bundle id}}')
-            # 스크롤
-            wd.execute_script('mobile:swipe', {'direction': 'up'})
+            sleep(3)
 
-            # xpath query
-            '//XCUIElementTypeButton[@name="찾을이름"]'
+            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="MY"]').click()
+            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="로그인·회원가입"]').click()
+            wd.find_element(AppiumBy.CLASS_NAME, 'XCUIElementTypeTextField').send_keys(id_29cm)
+            wd.find_element(AppiumBy.IOS_PREDICATE, 'value=="비밀번호"').send_keys(password_29cm)
+            wd.find_element(AppiumBy.ACCESSIBILITY_ID, '로그인하기').click()
 
-            # 요소 대기
-            wait = WebDriverWait(wd, 5)
-            wait.until(ec.visibility_of_element_located((AppiumBy.ID, ''))).click()
-            wait.until(ec.element_to_be_clickable((AppiumBy.XPATH, ''))).click()
-            sleep(1)
+            sleep(3)
+
 
         except Exception:
             # 스크린샷
