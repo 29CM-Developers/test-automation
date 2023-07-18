9e79b10578945a2d9d7ad628aebad1f9cd605b5d
mpark@29cm.co.kr <mpark@29cm.co.kr>
2023-07-18 17:33:07 +0900

    add mpark setup

--- android_automation/android_setup.py
+++ android_automation/android_setup.py
@@ -1,19 +1,22 @@
 from appium import webdriver
+from appium.webdriver.webdriver import AppiumOptions
 
 
 def mpark_setup():
-    android_caps = {}
-    android_caps['platformName'] = 'Android'
-    android_caps['platformVersion'] = '{{os version}}'
-    android_caps['deviceName'] = '{{device name}}'
-    android_caps['automationName'] = 'UIAutomator2'
-    android_caps['newCommandTimeout'] = 300
-    android_caps['appPackage'] = '{{apppackage_name}}'
-    android_caps['appActivity'] = '{{appactivity_name}}'
-    android_caps['noReset'] = True
-    android_caps['udid'] = '{{udid_string}}'
-    android_caps['goog:chromeOptions'] = {'androidPackage': 'app package name', 'androidUseRunningApp': True,
-                                          'w3c': False}
-    wd = webdriver.Remote('http://0.0.0.0:4723/wd/hub', capabilities=android_caps)
+    android_options = AppiumOptions()
+    android_options.set_capability("platformName", "Android")
+    android_options.set_capability("platformVersion", "13")
+    android_options.set_capability("deviceName", "S20")
+    android_options.set_capability("automationName", "UIAutomator2")
+    android_options.set_capability("appPackage", "com.the29cm.app29cm")
+    android_options.set_capability("appActivity", "com.the29cm.app29cm.intro.IntroActivity")
+    android_options.set_capability("udid", "R3CRC0NFQEZ")
+    android_options.set_capability("newCommandTimeout", 300)
+    android_options.set_capability("noReset", True)
+    android_options.set_capability("unlockType", "pin")
+    android_options.set_capability("unlockKey", "292929")
+    android_options.set_capability("goog:chromeOptions", {"androidPackage": "com.the29cm.app29cm", "androidUseRunningApp": True, "w3c": False})
 
-    return wd, android_caps
+    wd = webdriver.Remote('http://0.0.0.0:4723/wd/hub', options=android_options)
+
+    return wd, android_options
