e8dd62f9c81ac5767d919141183847819aa0bc9f
mpark@29cm.co.kr <mpark@29cm.co.kr>
2023-07-18 15:05:33 +0900

    jenkins test sample codes

--- android_automation/mpark_run_testcase.py
+++ android_automation/mpark_run_testcase.py
@@ -28,32 +28,35 @@ class AndroidTestAutomation(unittest.TestCase):
         self.wd, self.and_cap = mpark_setup()
         self.wd.implicitly_wait(5)
 
+        # report data
+        self.count = 0
+        self.total_time = ''
+        self.slack_result = ''
+
     def tearDown(self):
         try:
             self.wd.close_app()
-            print('앱 종료 실행')
             self.wd.quit()
-            print('wd quit 실행')
-            time.sleep(2)
             self.appium.stop()
-            print('appium service stop 성공')
         except Exception:
             self.appium.stop()
-            print('exception')
 
     def test_sample_def_name(self):
         # 테스트 자동화 실행 return값을 self.result_data에 넣으면 해당 값들을 가지고 slack noti를 보내게 됩니다
         self.def_name = sys._getframe().f_code.co_name
 
-        # 실제 실행
         self.result_data = AutomationTesting.default_test(self, self.wd)
-
         self.response = slack_result_notifications.slack_notification(self)
-        slack_result_notifications.slack_thread_notification(self)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
 
-        # 실제 실행
+        self.result_data = AutomationTesting.default_fail_test(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
         self.result_data = AutomationTesting.default_test(self, self.wd)
-        slack_result_notifications.slack_thread_notification(self)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
 
 
     def test_pass(self):
