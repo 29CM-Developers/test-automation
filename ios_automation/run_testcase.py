3a7d3e394e5a03ab8e0a00cc8d999e05b97a23c2
dajeong <dajjeong@29cm.co.kr>
2023-07-18 13:36:23 +0900

    sample test

--- ios_automation/run_testcase.py
+++ ios_automation/run_testcase.py
@@ -1,8 +1,12 @@
 import logging
+import sys
 import time
 import unittest
 
+import requests
 from appium.webdriver.appium_service import AppiumService
+
+from com_utils import slack_result_notifications
 from ios_setup import dajjeong_setup
 from ios_automation.test_cases.sample_test import AutomationTesting
 from selenium.common.exceptions import NoSuchElementException, InvalidSessionIdException
@@ -26,6 +30,15 @@ class IOSTestAutomation(unittest.TestCase):
         self.wd, self.iOS_cap = dajjeong_setup()
         self.wd.implicitly_wait(3)
 
+        user_info = requests.get(f"http://192.168.103.13:50/qa/personal/mpark")
+        self.pconf = user_info.json()
+        public_info = requests.get(f"http://192.168.103.13:50/qa/personal/info")
+        self.conf = public_info.json()
+
+        self.count = 0
+        self.total_time = ''
+        self.slack_result = ''
+
     def tearDown(self):
         try:
             self.wd.terminate_app('kr.co.29cm.App29CM')
@@ -39,8 +52,26 @@ class IOSTestAutomation(unittest.TestCase):
         except InvalidSessionIdException:
             self.appium.stop()
 
-    def test_login(self):
-        AutomationTesting.login_test(self, self.wd)
+    def test_dajjeong_login(self):
+
+        self.def_name = sys._getframe().f_code.co_name
+
+        # 1번째 테스트
+        self.result_data = AutomationTesting.dajjeong_test(self, self.wd)
+        # slack noti 작성 - 1회만 필요. ts값이 필요하여 self.response에 반환값 저장
+        self.response = slack_result_notifications.slack_notification(self)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        # 2번째 테스트
+        self.result_data = AutomationTesting.scroll_test(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        self.result_data = AutomationTesting.error_test(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
 
     def test_error_id_login(self):
         AutomationTesting.id_error_test(self, self.wd)
