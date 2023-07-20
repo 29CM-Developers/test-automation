8344eb5de238b934d446974fa01e94622652e235
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-07-20 19:14:13 +0900

    홍해진 id값 찾을때까지 스크롤 동작 메소드 추가 scroll_to_element(wd, element_id)

--- com_utils/element_control.py
+++ com_utils/element_control.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
-
-
+from appium.webdriver.common.touch_action import TouchAction
+from appium.webdriver.common.mobileby import MobileBy
+import time
+import logging
 def class_find_click(wd, class_name):
     wd.find_element(By.CLASS_NAME, f'{class_name}').click()
 
@@ -35,3 +37,20 @@ def css_find(wd, css):
 
 def tag_find(wd, tag):
     wd.find_element(By.CSS_SELECTOR, f'{tag}')
+
+def 가scroll_to_element(wd, element_id):
+    logging.basicConfig(level=logging.INFO)
+    while True:
+        try:
+            # 원하는 요소를 찾으면 스크롤 종료
+            element = wd.find_element(MobileBy.ID, element_id)
+            break
+        except:
+            # 요소를 찾지 못하면 아래로 스크롤
+            size = wd.get_window_size()
+            start_x = size["width"] / 2
+            start_y = size["height"] * 0.8
+            end_y = size["height"] * 0.2
+            duration_ms = 1000  # 스크롤 동작 시간 (밀리초)
+            wd.swipe(start_x, start_y, start_x, end_y, duration_ms)
+            logging.info("스크롤햇도")
