102ad5b87f63371c2a719558101e617683f8990d
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-07-20 19:27:55 +0900

    홍해진 불필요한 내용 제거 주석 제거

--- android_automation/test_cases/loginpage_test.py
+++ android_automation/test_cases/loginpage_test.py
@@ -57,6 +57,10 @@ class LoginLogout:
                 test_result = 'FAIL'
             logging.info("로그아웃 문구 확인 : %s " % logout_check.text)
 
+            # 하단 네비게이터에 MY 메뉴 진입
+            wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'HOME').click()
+            logging.info("홈화면 진입")
+
         except Exception:
             # 오류 발생 시 테스트 결과를 실패로 한다
             test_result = 'FAIL'
@@ -189,134 +193,7 @@ class LoginLogout:
             # 값 재사용 용이성을 위해 dict로 반환한다
             result_data = {'test_result': test_result, 'error_texts': error_texts, 'img_src': img_src,'test_name': test_name, 'run_time': run_time}
             return result_data
-    def login_page_entered(self, wd, error_text=''):
 
-        try:
-            logging.info("---------1.로그인 페이지 진입 테스트 시작")
-            sleep(5)
-            #하단 네비게이터에 MY 메뉴 진입
-            wd.find_element(AppiumBy.ACCESSIBILITY_ID,'MY').click()
-            logging.info("홈 > 마이페이지 화면 진입")
-
-            #로그인 회원가입 버튼 선택
-            wd.find_element(AppiumBy.ID, LoginPageLocators.my_page_not_login).click()
-            logging.info("로그인 버튼 선택")
-            sleep(3)
-
-            #로그인 화면 진입 확인
-            login_page_title = wd.find_element(By.XPATH, LoginPageLocators.login_page_title)
-
-            logging.info("가이드 문구 : %s " % login_page_title.text)
-            return login_page_title.text
-
-        except Exception:
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc()
-            return error_text
-    def id_password_not_entered(self, wd, error_text=''):
-
-        try:
-            logging.info("---------2.로그인 미입력 케이스 시작")
-            sleep(3)
-            # 미입력 후 로그인 하기 버튼 선택
-            wd.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-            logging.info("로그인 버튼 선택")
-
-            #가이드 문구 확인 - 아이디를 입력해주세요
-            guide_text = wd.find_element(By.XPATH, LoginPageLocators.guide_text)
-
-            logging.info("가이드 문구 : %s " % guide_text.text)
-            return guide_text.text
-
-        except Exception:
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc()
-            return error_text
-    def id_not_entered(self, wd, error_text=''):
-
-        try:
-            logging.info("---------3.아이디 미입력 케이스 시작")
-            sleep(5)
-            # 비밀번호 입력 후 로그인 하기 버튼 선택
-            wd.find_element(By.XPATH, LoginPageLocators.password_field).send_keys(config.LOGIN_SUCCESS_PW)
-            logging.info("패스워드 필드 입력")
-            wd.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-            logging.info("로그인 버튼 선택")
-            #가이드 문구 확인 - 아이디를 입력해주세요
-            guide_text = wd.find_element(By.XPATH, LoginPageLocators.guide_text)
-            wd.find_element(By.XPATH, LoginPageLocators.password_field).send_keys("")
-            logging.info("가이드 문구 : %s " % guide_text.text)
-            return guide_text.text
-
-        except Exception:
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc()
-            return error_text
-    def password_not_entered(self, wd, error_text=''):
-
-        try:
-            logging.info("---------4.비밀번호 미입력 케이스 시작")
-            sleep(5)
-            #아이디 입력 후 로그인 하기 버튼 선택
-            wd.find_element(By.XPATH, LoginPageLocators.id_field).send_keys(config.LOGIN_SUCCESS_ID)
-            logging.info("아이디 필드 입력")
-            wd.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-            logging.info("로그인 버튼 선택")
-
-            #가이드 문구 확인 - 비밀번호를 입력하세요
-            guide_text = wd.find_element(By.XPATH, LoginPageLocators.guide_text)
-
-            logging.info("가이드 문구 : %s " % guide_text.text)
-            return guide_text.text
-
-        except Exception:
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc()
-            return error_text
-    def enter_wrong_password(self, wd, error_text=''):
-
-        try:
-            logging.info("---------5.잘못된 비밀번호 입력 케이스 시작")
-            sleep(5)
-            # 잘못된 비밀번호 입력 후 로그인 하기 버튼 선택
-            wd.find_element(By.XPATH, LoginPageLocators.id_field).send_keys(config.LOGIN_SUCCESS_ID)
-            wd.find_element(By.XPATH, LoginPageLocators.password_field).send_keys(config.LOGIN_FAILED_PW)
-            wd.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-            logging.info("로그인 버튼 선택")
-            # 로그인 실패 문구 확인
-            guide_text = wd.find_element(By.XPATH, LoginPageLocators.wrong_password_guide_text)
-
-            logging.info("가이드 문구 : %s " % guide_text.text)
-
-            # 로그인 6회 실패
-            # 로그인 제한 확인 - 10 분 제한으로 자동화에서 미진행
-
-            return guide_text.text
-
-        except Exception:
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc()
-            return error_text
     def Login_with_SNS(self, wd, error_text=''):
 
         try:
@@ -336,35 +213,6 @@ class LoginLogout:
 
             logging.info("가이드 문구 : %s " % guide_text.text)
             return guide_text.text
-
-        except Exception:
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc()
-            return error_text
-    def enter_the_correct_password(self, wd, error_text=''):
-
-        try:
-            logging.info("---------7.올바른 비밀번호 입력 케이스 시작")
-            sleep(5)
-
-            # 올바른 비밀번로 입력 후 로그인 하기 버튼 선택
-            wd.find_element(By.XPATH, LoginPageLocators.id_field).send_keys(config.LOGIN_SUCCESS_ID)
-            wd.find_element(By.XPATH, LoginPageLocators.password_field).send_keys(config.LOGIN_SUCCESS_PW)
-            wd.find_element(By.XPATH, LoginPageLocators.login_btn).click()
-            logging.info("로그인 버튼 선택")
-            sleep(3)
-
-            # 로그인 성공 진입 확인
-            login_name = wd.find_element(By.ID, LoginPageLocators.my_page_login_name)
-
-            logging.info("로그인 유저 이름 : %s " % login_name.text)
-            #홈화면 이동
-
-            return login_name.text
 
         except Exception:
             # 스크린샷
