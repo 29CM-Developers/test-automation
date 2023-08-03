ed650362532c5c3267a32057ca89bb59bca59ee4
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-08-03 18:01:07 +0900

    홈 배너, 컨텐츠 시나리오 변경 api 호출하여 데이터 확인 시나리오로 변경 warn 이슈 노출되도록 수정

--- android_automation/test_cases/home_test.py
+++ android_automation/test_cases/home_test.py
@@ -37,19 +37,43 @@ class Home:
             sleep(2)
             wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'HOME').click()
             print("홈 탭 선택")
-            txtFeedBannerTitle1 = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtFeedBannerTitle').text
-            print(f"현재 피드 타이틀 : {txtFeedBannerTitle1} ")
-            element_control.swipe_left_to_right(wd)
-            print("왼쪽에서 오른쪽으로 스와이프")
-            txtFeedBannerTitle2 = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtFeedBannerTitle').text
-            print(f"스와이프 후 피드 타이틀 : {txtFeedBannerTitle2} ")
-            if txtFeedBannerTitle1 != txtFeedBannerTitle2:
-                print("상단 배너 좌우 슬라이드 확인")
+
+            response = requests.get("https://content-api.29cm.co.kr/api/v4/banners?bannerDivision=HOME_MOBILE")
+            if response.status_code == 200:
+                api_data = response.json()
+                sleep(3)
+                # 첫 번째 "feedTitle" 데이터 가져오기
+                if api_data["data"]["bannerList"]:
+                    fifth_banner_title = api_data["data"]["bannerList"][4]["bannerTitle"]
+                    print(f"홈 상단 배너 api 호출 하여 다섯번째 bannerTitle 저장: {fifth_banner_title}")
+                else:
+                    print("데이터가 없습니다.")
+
+                count = 0
+                found_element = None
+                while count <= 10:
+                    try:
+                        # 두번째 피드 타이틀와 일치하는 요소 찾기
+                        element = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtFeedBannerTitle')
+                        if element.text == fifth_banner_title:
+                            found_element = element
+                            print(f"다섯번째 bannerTitle 발견 : {fifth_banner_title}")
+                            print(f"배너타이틀 노출 확인 : {fifth_banner_title}")
+                            break
+                    except NoSuchElementException:
+                        pass
+                    # 스와이프 액션 수행
+                    element_control.swipe_right_to_left(wd)
+                    ++count
+                if found_element is None:
+                    test_result = 'WARN'
+                    warning_texts.append("홈화면 상단 배너 좌우 슬라이드 확인 실패")
+
             else:
-                print("상단 배너 좌우 슬라이드 확인 실패")
+                print("API 호출에 실패했습니다.")
+
             home_title = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtShowAll').text
             wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtShowAll').click()
-            next_layer = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/rootView')
             print("모아보기 화면 진입")
             next_layer_title = wd.find_element(AppiumBy.XPATH,'//*[@resource-id="__next"]/android.widget.TextView[@index=0]').text
             if next_layer_title == home_title:
@@ -57,6 +81,8 @@ class Home:
                 print("모아보기 버튼 선택하여 모아보기 페이지 진입 확인")
             else:
                 print(f"모아보기 진입 확인 실패 : {next_layer_title} 노출")
+                test_result = 'WARN'
+                warning_texts.append("모아보기 페이지 진입 확인 실패")
             # 뒤로가기로 홈화면 진입 확인
             wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/imgBack').click()
             print("뒤로가기 선택")
@@ -73,6 +99,8 @@ class Home:
                 print(f"선물하기 타이틀 확인 : {gift_title}")
             else :
                 print(f"선물하기 타이틀 확인 실패 : {gift_title}")
+                test_result = 'WARN'
+                warning_texts.append("다이나믹 게이트 타이틀 확인 실패")
 
             # 뒤로가기로 홈화면 진입 확인
             gift_layer.find_element(AppiumBy.XPATH,'//android.view.View[1]/android.view.View/android.view.View/android.widget.Button').click()
@@ -80,16 +108,6 @@ class Home:
             print("뒤로가기 선택")
             sleep(3)
             print("[홈화면 배너 확인]CASE 종료")
-
-            # 확인 실패 시, 테스트 종료되지는 않지만 테스트 종료 후 확인이 필요한 경우
-            # 테스트 결과 WARN으로 변경 -> 예시
-            # 확인이 필요한 지점에 코드 작성
-            # test_scenario = '확인할 테스트 시나리오 (ex. 홈화면 피드 상품의 좋아요 버튼 선택)'
-            # try:
-            #     testcode
-            # except NoSuchElementException:
-            #     test_result = 'WARN'
-            #     warning_texts.append(test_scenario)
 
         except Exception:
             # 오류 발생 시 테스트 결과를 실패로 한다
@@ -117,7 +135,7 @@ class Home:
                 'test_name': test_name, 'run_time': run_time}
             return result_data
 
-    def test_home_contents(self, wd, test_result='PASS', error_texts=[], img_src=''):
+    def test_home_contents(self, wd, test_result='PASS', error_texts=[], img_src='', warning_texts=[]):
 
         # 현재 함수명 저장 - slack noti에 사용
         test_name = self.dconf[sys._getframe().f_code.co_name]
@@ -127,10 +145,7 @@ class Home:
             print("[홈화면 컨텐츠 확인]CASE 시작")
             sleep(5)
             tab_layer = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/tabLayout')
-            rec_tab_title = tab_layer.find_element(AppiumBy.XPATH, '//android.view.ViewGroup[4]/android.view.ViewGroup/android.widget.TextView').text
-            women_tab_title = tab_layer.find_element(AppiumBy.XPATH, '//android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView').text
             # 2. 홈 > 피드 > 추천 탭선택
-            # tab_title_elements = wd.find_elements(AppiumBy.XPATH, '//*[@resource-id="com.the29cm.app29cm:id/tabTitle"]')
             tab_layer.find_element(AppiumBy.XPATH,'//android.view.ViewGroup[4]/android.view.ViewGroup/android.widget.TextView').click()
             print('홈 > 피드 > 추천 탭선택 ')
             guide_text = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/textRecommend')
@@ -139,6 +154,8 @@ class Home:
                 print(f"'{self.pconf['NAME']}님을 위한 추천 상품’ 가이드 문구 노출 확인")
             else:
                 print(f"'{self.pconf['NAME']}님을 위한 추천 상품’ 가이드 문구 노출 실패")
+                test_result = 'WARN'
+                warning_texts.append("홈화면 추천 탭 타이틀 확인 실패")
             print(f"가이드 문구 : {guide_text.text} ")
 
             tab_layer.find_element(AppiumBy.XPATH,'//android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView').click()
@@ -153,6 +170,7 @@ class Home:
             # 문자열을 정수로 변환
             before_like_count = int(before_like_count)
             products_layer.find_element(AppiumBy.XPATH,'//android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.widget.ImageView').click()
+            sleep(1)
             #좋아요 선택
             after_like_count = products_layer.find_element(AppiumBy.XPATH, '//android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.widget.TextView').text
             # 좋아요 누른  좋아요 갯수 확인
@@ -162,9 +180,11 @@ class Home:
             after_like_count = int(after_like_count)
             sleep(3)
             if after_like_count == before_like_count+1 :
-                print(f"갯수 확인 성공1 : 좋아요 누르기 전 갯수 -  {before_like_count}, 좋아요 누른 후 갯수 - {after_like_count}")
+                print(f"갯수 확인 성공 : 좋아요 누르기 전 갯수 -  {before_like_count}, 좋아요 누른 후 갯수 - {after_like_count}")
             else :
-                print(f"갯수 확인 실패1 : 좋아요 누르기 전 갯수 -  {before_like_count}, 좋아요 누른 후 갯수 - {after_like_count}")
+                print(f"갯수 확인 실패 : 좋아요 누르기 전 갯수 -  {before_like_count}, 좋아요 누른 후 갯수 - {after_like_count}")
+                test_result = 'WARN'
+                warning_texts.append("피드 아이템 좋아요 개수 증가 확인 실패")
 
             before_like_count = products_layer.find_element(AppiumBy.XPATH,'//android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.widget.TextView').text
 
@@ -183,9 +203,11 @@ class Home:
             after_like_count = int(after_like_count)
             sleep(3)
             if after_like_count == before_like_count - 1:
-                print(f"갯수 확인 성공2 : 좋아요 취소 누르기 전 갯수 -  {before_like_count}, 좋아요 취소 누른 후 갯수 - {after_like_count}")
+                print(f"갯수 확인 성공 : 좋아요 취소 누르기 전 갯수 -  {before_like_count}, 좋아요 취소 누른 후 갯수 - {after_like_count}")
             else:
-                print(f"갯수 확인 실패2 : 좋아요 취소 누르기 전 갯수 -  {before_like_count}, 좋아요 취소 누른 후 갯수 - {after_like_count}")
+                print(f"갯수 확인 실패 : 좋아요 취소 누르기 전 갯수 -  {before_like_count}, 좋아요 취소 누른 후 갯수 - {after_like_count}")
+                test_result = 'WARN'
+                warning_texts.append("피드 아이템 좋아요 개수 차감 확인 실패")
             sleep(5)
             # API 호출
             response = requests.get("https://content-api.29cm.co.kr/api/v5/feeds?experiment=&feed_sort=WOMEN&home_type=APP_HOME&limit=10&offset=0")
@@ -194,18 +216,34 @@ class Home:
                 sleep(3)
                 # 첫 번째 "feedTitle" 데이터 가져오기
                 if api_data["data"]["results"]:
-                    first_feed_title = api_data["data"]["results"][0]["feedTitle"]
-                    print(f"우먼탭 api 호출 하여 첫번째 feedTitle 저장")
+                    second_feed_title = api_data["data"]["results"][7]["feedTitle"]
+                    print(f"우먼탭 api 호출 하여 두번째 feedTitle 저장: {second_feed_title}")
                 else:
                     print("데이터가 없습니다.")
-                app_feedTitle = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtFeedTitle').text
-                print(f"확인3 : 우먼탭에 첫번째로 노출되는 피드타이틀과 api 호출한 feedTitle 동일한지 확인")
-                if app_feedTitle == first_feed_title :
-                    print(f"피드 타이틀 동일 확인 : {app_feedTitle}")
-                else :
-                    print(f"피드 타이틀 동일 확인 실패 : {app_feedTitle}")
+
+                count = 0
+                found_element = None
+                while count <= 10:
+                    try:
+                        # 두번째 피드 타이틀와 일치하는 요소 찾기
+                        element = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/txtFeedTitle')
+                        if element.text == second_feed_title:
+                            found_element = element
+                            print(f"두번째 feedTitle 발견 : {second_feed_title}")
+                            print(f"피드 컨텐츠 추가 노출 확인 : {second_feed_title}")
+                            break
+                    except NoSuchElementException:
+                        pass
+                    # 스크롤 액션 수행
+                    element_control.scroll(wd)
+                    ++count
+                if found_element is None:
+                    test_result = 'WARN'
+                    warning_texts.append("피드 컨텐츠 추가 노출 확인 실패")
+
             else:
                 print("API 호출에 실패했습니다.")
+            sleep(2)
             print("[홈화면 컨텐츠 확인]CASE 종료")
 
 
