94e173b5643598239f8d43adb148f1a4c45eecd7
dajeong <dajjeong@29cm.co.kr>
2023-08-08 18:09:40 +0900

    iOS app terminate 코드 테스트폰 대상으로 변경

--- ios_automation/ios_app_terminate.py
+++ ios_automation/ios_app_terminate.py
@@ -1,19 +1,33 @@
 import unittest
 
 from appium.webdriver.appium_service import AppiumService
-from ios_automation.ios_setup import dajjeong_setup
+from ios_automation.ios_setup import pro12_setup, pro14_setup
 
 
 class IOSAppTerminate(unittest.TestCase):
-    def test_ios_app_terminate(self):
-        # 포트번호와 셋업은 세팅에 맞게 변경해주세요
-
+    def test_pro12_app_terminate(self):
         self.appium = AppiumService()
-        self.appium.start(args=['-p', '4924', '--base-path', '/wd/hub', '--default-capabilities',
+        self.appium.start(args=['-p', '4944', '--base-path', '/wd/hub', '--default-capabilities',
                                 '{"appium:chromedriverExecutable": "/usr/local/bin"}'])
 
         # webdriver
-        self.wd, self.iOS_cap = dajjeong_setup()
+        self.wd, self.iOS_cap = pro12_setup()
+        self.wd.implicitly_wait(3)
+
+        # 앱, wd, appium 순차 종료
+        self.wd.terminate_app('kr.co.29cm.App29CM')
+        self.wd.quit()
+        self.appium.stop()
+
+    def test_pro14_app_terminate(self):
+        # 포트번호와 셋업은 세팅에 맞게 변경해주세요
+
+        self.appium = AppiumService()
+        self.appium.start(args=['-p', '4943', '--base-path', '/wd/hub', '--default-capabilities',
+                                '{"appium:chromedriverExecutable": "/usr/local/bin"}'])
+
+        # webdriver
+        self.wd, self.iOS_cap = pro14_setup()
         self.wd.implicitly_wait(3)
 
         # 앱, wd, appium 순차 종료
