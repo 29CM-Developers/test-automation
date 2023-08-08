677df0ba8a096b4e0740f7104e36e30e67b605e8
dajeong <dajjeong@29cm.co.kr>
2023-08-08 12:03:42 +0900

    홈화면 배너와 컨텐츠 스크립트의 에러 발생 원인 수정

--- ios_automation/test_cases/home_test.py
+++ ios_automation/test_cases/home_test.py
@@ -39,20 +39,23 @@ class Home:
                 # 홈화면 배너 타이틀 모두 저장
                 banner_home = []
                 for i in range(0, banner_count):
-                    sleep(1.5)
+                    sleep(2)
                     try:
-                        banner_title = wd.find_element(AppiumBy.XPATH,
-                                                            '//XCUIElementTypeCollectionView[@index="4"]/XCUIElementTypeCell[1]/XCUIElementTypeOther')
-                        banner_title_text = banner_title.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@index="1"]').text
+                        banner_title_text = wd.find_element(AppiumBy.XPATH,
+                                                            '//XCUIElementTypeCollectionView[@index="4"]/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]').text
                         banner_home.append(banner_title_text)
+                        print(banner_title_text)
+                    except NoSuchElementException:
+                        print("타이틀 없는 배너")
+                        pass
                     except Exception:
-                        print('타이틀 확인 실패하여 이전 배너로 스와이프')
                         # 에러 발생하여 타이틀 확인 실패 시, 이전 배너로 스와이프하여 타이틀 저장
                         banner = wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeCollectionView')
                         com_utils.element_control.swipe_left_to_right(wd, banner)
                         banner_title_text = wd.find_element(AppiumBy.XPATH,
-                                                        '//XCUIElementTypeCollectionView[@index="4"]/XCUIElementTypeCell[1]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]').text
+                                                            '//XCUIElementTypeCollectionView[@index="4"]/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]').text
                         banner_home.append(banner_title_text)
+                        print(banner_title_text)
 
                 # API 호출 배너 리스트와 저장된 홈 배너 리스트 비교 (저장한 홈 배너 리스트 안에 호출한 리스트가 포함되면 pass)
                 if set(banner_api).issubset(set(banner_home)):
@@ -119,61 +122,78 @@ class Home:
             # 우먼 카테고리 탭 선택
             wd.find_element(AppiumBy.ACCESSIBILITY_ID, '우먼').click()
 
-            # 첫번째 피드의 첫번째 상품의 좋아요 xpath와 좋아요 수 xpath
-            content_like_xpath = '//XCUIElementTypeCollectionView/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeOther[@index="2"]/XCUIElementTypeButton[@index="4"]'
-            content_like_count_xpath = f'{content_like_xpath}/XCUIElementTypeStaticText'
-
-            # 좋아요 버튼 선택 전, 좋아요 수 저장
-            content_like_count = wd.find_element(AppiumBy.XPATH, content_like_count_xpath).text
-            content_like_count = int(content_like_count.replace(',', ''))
-
-            # 좋아요 버튼 선택 -> 찜하기 등록
-            wd.find_element(AppiumBy.XPATH, content_like_xpath).click()
-            content_like_select = wd.find_element(AppiumBy.XPATH, content_like_count_xpath).text
-            content_like_select = int(content_like_select.replace(',', ''))
-            if content_like_select == content_like_count+1:
-                print('피드 아이템 좋아요 개수 증가 확인')
-                pass
-            else:
-                test_result = 'WARN'
-                warning_texts.append('피드 아이템 좋아요 개수 증가 확인 실패')
-                print('피드 아이템 좋아요 개수 증가 확인 실패')
-
-            # 좋아요 버튼 선택 -> 찜하기 해제
-            wd.find_element(AppiumBy.XPATH, content_like_xpath).click()
-            content_like_select = wd.find_element(AppiumBy.XPATH, content_like_count_xpath).text
-            content_like_select = int(content_like_select.replace(',', ''))
-            if content_like_select == content_like_count:
-                print('피드 아이템 좋아요 개수 차감 확인')
-                pass
-            else:
-                test_result = 'WARN'
-                warning_texts.append('피드 아이템 좋아요 개수 차감 확인 실패')
-                print('피드 아이템 좋아요 개수 차감 확인 실패')
-
-            # 홈화면 우먼 탭 컨텐츠 API 호출하여 2번째 contents 피드 정보 저장
+            # 홈 배너 API 호출
             response = requests.get(
                 'https://content-api.29cm.co.kr/api/v5/feeds?experiment=&feed_sort=WOMEN&home_type=APP_HOME&limit=20&offset=0')
             if response.status_code == 200:
                 contents_api_data = response.json()
                 feed_item_contents = contents_api_data['data']['results']
 
-                # feedType이 contents인 두번째 피드 정보 저장
-                global feed_contents_api
-                for i in range(1, 10):
+                # feedType이 contents인 첫번째, 두번째 텍스트 저장
+                feed_contents_api = []
+                for i in range(0, 10):
                     feed_contents_type = feed_item_contents[i]['feedType']
                     if feed_contents_type == 'contents':
-                        feed_contents_api = feed_item_contents[i]['feedContents']
-                        break
+                        feed_contents_text = feed_item_contents[i]['feedContents']
+                        feed_contents_api.append(feed_contents_text)
                     else:
                         pass
+                feed_contents_1st = feed_contents_api[0]
+                feed_contents_2nd = feed_contents_api[1]
 
-                # 저장한 피드 정보와 동일한 피드 정보가 노출 될 때까지 스크롤
+                # 저장한 첫번째 피드 정보와 동일한 피드 정보가 노출 될 때까지 스크롤
                 for i in range(0, 10):
                     try:
                         feed_contents = wd.find_element(AppiumBy.XPATH,
-                                               '//XCUIElementTypeCollectionView/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeOther[@index="1"]/XCUIElementTypeStaticText').text
-                        if feed_contents_api == feed_contents:
+                                                        '//XCUIElementTypeCollectionView/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeOther[@index="1"]/XCUIElementTypeStaticText').text
+                        if feed_contents_1st == feed_contents:
+                            print('첫번째 피드 컨텐츠 노출 확인')
+                            break
+                        else:
+                            wd.execute_script('mobile:swipe', {'direction': 'up'})
+                            print('첫번째 피드 확인 안되어 스크롤')
+                            i += 1
+                    except NoSuchElementException:
+                        wd.execute_script('mobile:swipe', {'direction': 'up'})
+
+                # 첫번째 피드의 첫번째 상품의 좋아요 xpath와 드아요 수 xpath
+                content_like_xpath = '//XCUIElementTypeCollectionView/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeOther[@index="2"]/XCUIElementTypeButton[@index="4"]'
+                content_like_count_xpath = f'{content_like_xpath}/XCUIElementTypeStaticText'
+
+                # 좋아요 버튼 선택 전, 좋아요 수 저장
+                content_like_count = wd.find_element(AppiumBy.XPATH, content_like_count_xpath).text
+                content_like_count = int(content_like_count.replace(',', ''))
+
+                # 좋아요 버튼 선택 -> 찜하기 등록
+                wd.find_element(AppiumBy.XPATH, content_like_xpath).click()
+                content_like_select = wd.find_element(AppiumBy.XPATH, content_like_count_xpath).text
+                content_like_select = int(content_like_select.replace(',', ''))
+                if content_like_select == content_like_count + 1:
+                    print('피드 아이템 좋아요 개수 증가 확인')
+                    pass
+                else:
+                    test_result = 'WARN'
+                    warning_texts.append('피드 아이템 좋아요 개수 증가 확인 실패')
+                    print('피드 아이템 좋아요 개수 증가 확인 실패')
+
+                # 좋아요 버튼 선택 -> 찜하기 해제
+                wd.find_element(AppiumBy.XPATH, content_like_xpath).click()
+                content_like_select = wd.find_element(AppiumBy.XPATH, content_like_count_xpath).text
+                content_like_select = int(content_like_select.replace(',', ''))
+                if content_like_select == content_like_count:
+                    print('피드 아이템 좋아요 개수 차감 확인')
+                    pass
+                else:
+                    test_result = 'WARN'
+                    warning_texts.append('피드 아이템 좋아요 개수 차감 확인 실패')
+                    print('피드 아이템 좋아요 개수 차감 확인 실패')
+
+                # 저장한 두번째 피드 정보와 동일한 피드 정보가 노출 될 때까지 스크롤
+                for i in range(0, 10):
+                    try:
+                        feed_contents = wd.find_element(AppiumBy.XPATH,
+                                                        '//XCUIElementTypeCollectionView/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeOther[@index="1"]/XCUIElementTypeStaticText').text
+                        if feed_contents_2nd == feed_contents:
                             print('피드 컨텐츠 추가 노출 확인')
                             break
                         else:
@@ -182,6 +202,7 @@ class Home:
                             i += 1
                     except NoSuchElementException:
                         wd.execute_script('mobile:swipe', {'direction': 'up'})
+
             else:
                 test_result = 'WARN'
                 warning_texts.append('피드 컨텐츠 API 불러오기 실패')
@@ -207,14 +228,3 @@ class Home:
                 'test_result': test_result, 'error_texts': error_texts, 'img_src': img_src,
                 'test_name': test_name, 'run_time': run_time, 'warning_texts': warning_points}
             return result_data
-
-
-
-
-
-
-
-
-
-
-
