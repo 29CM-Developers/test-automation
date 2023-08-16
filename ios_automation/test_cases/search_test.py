d62e05de7c3a855119f603c5309e3e1fd2bfa014
dajeong <dajjeong@29cm.co.kr>
2023-08-16 13:26:37 +0900

    API 호출 실패 할 경우의 코드 작성

--- ios_automation/test_cases/search_test.py
+++ ios_automation/test_cases/search_test.py
@@ -24,48 +24,51 @@ class Search:
             wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeButton[`label == "SEARCH"`]').click()
 
             # 전체 기준 인기검색어 리스트 호출
-            reponse = requests.get('https://search-api.29cm.co.kr/api/v4/keyword/popular?limit=100&brandLimit=30')
-            all_popular_data = reponse.json()
-            all_popular_brand_30th = all_popular_data['data']['popularBrandKeywords'][29]
+            response = requests.get('https://search-api.29cm.co.kr/api/v4/keyword/popular?limit=100&brandLimit=30')
+            if response.status_code == 200:
+                all_popular_data = response.json()
+                all_popular_brand_30th = all_popular_data['data']['popularBrandKeywords'][29]
 
-            # 인기 브랜드 타이틀 확인
-            try:
-                wd.find_element(AppiumBy.ACCESSIBILITY_ID, '지금 많이 찾는 브랜드')
-                print('인기 브랜드 타이틀 확인')
-            except NoSuchElementException:
-                test_result = 'WARN'
-                warning_texts.append('인기 브랜드 타이틀 확인 실패')
-                print('인기 브랜드 타이틀 확인 실패')
+                # 인기 브랜드 타이틀 확인
+                try:
+                    wd.find_element(AppiumBy.ACCESSIBILITY_ID, '지금 많이 찾는 브랜드')
+                    print('인기 브랜드 타이틀 확인')
+                except NoSuchElementException:
+                    test_result = 'WARN'
+                    warning_texts.append('인기 브랜드 타이틀 확인 실패')
+                    print('인기 브랜드 타이틀 확인 실패')
 
-            # 인기 브랜드 30위 확인
-            popular_brand = wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeCollectionView[@index="2"]')
-            com_utils.element_control.swipe_control(wd, popular_brand, 'left', 30)
-            com_utils.element_control.swipe_control(wd, popular_brand, 'left', 30)
+                # 인기 브랜드 30위 확인
+                popular_brand = wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeCollectionView[@index="2"]')
+                com_utils.element_control.swipe_control(wd, popular_brand, 'left', 30)
+                com_utils.element_control.swipe_control(wd, popular_brand, 'left', 30)
 
-            popular_brand_30th = popular_brand.find_element(AppiumBy.XPATH,
-                                                            '//XCUIElementTypeCell[@index="9"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]')
-            popular_brand_30th_name = popular_brand_30th.text
+                popular_brand_30th = popular_brand.find_element(AppiumBy.XPATH,
+                                                                '//XCUIElementTypeCell[@index="9"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]')
+                popular_brand_30th_name = popular_brand_30th.text
 
-            # API 호출한 인기브랜드 30위와 실제 30위가 동일한지 확인
-            if all_popular_brand_30th == popular_brand_30th_name:
-                print('인기 브랜드 순위 확인')
+                # API 호출한 인기브랜드 30위와 실제 30위가 동일한지 확인
+                if all_popular_brand_30th == popular_brand_30th_name:
+                    print('인기 브랜드 순위 확인')
+                else:
+                    test_result = 'WARN'
+                    warning_texts.append('인기 브랜드 순위 확인 실패')
+                    print('인기 브랜드 순위 확인 실패')
+
+                # 인기 브랜드 30위 검색
+                popular_brand_30th.click()
+
+                # 검색 결과 화면의 브랜드명에 검색어와 연관된 브랜드 확인
+                search_result_brand = wd.find_element(AppiumBy.XPATH,
+                                                      '//XCUIElementTypeCollectionView/XCUIElementTypeCell/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="0"]')
+                if popular_brand_30th_name in search_result_brand.text:
+                    print('인기 브랜드 검색 확인')
+                else:
+                    test_result = 'WARN'
+                    warning_texts.append('인기 브랜드 검색 확인 실패')
+                    print('인기 브랜드 검색 확인 실패')
             else:
-                test_result = 'WARN'
-                warning_texts.append('인기 브랜드 순위 확인 실패')
-                print('인기 브랜드 순위 확인 실패')
-
-            # 인기 브랜드 30위 검색
-            popular_brand_30th.click()
-
-            # 검색 결과 화면의 브랜드명에 검색어와 연관된 브랜드 확인
-            search_result_brand = wd.find_element(AppiumBy.XPATH,
-                                                  '//XCUIElementTypeCollectionView/XCUIElementTypeCell/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="0"]')
-            if popular_brand_30th_name in search_result_brand.text:
-                print('인기 브랜드 검색 확인')
-            else:
-                test_result = 'WARN'
-                warning_texts.append('인기 브랜드 검색 확인 실패')
-                print('인기 브랜드 검색 확인 실패')
+                print('인기 검색어 API 호출 실패')
 
             # 검색 화면으로 복귀
             wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'icNavigationbarBackBlack').click()
@@ -84,27 +87,30 @@ class Search:
             wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeButton[`label == "적용하기"`]').click()
 
             # 변경된 필터 기준
-            reponse = requests.get('https://search-api.29cm.co.kr/api/v4/keyword/popular?limit=100&brandLimit=30&group=female&ageGroup=30to34')
-            filter_popular_data = reponse.json()
-            filter_popular_brand_10th = filter_popular_data['data']['popularBrandKeywords'][9]
+            response = requests.get('https://search-api.29cm.co.kr/api/v4/keyword/popular?limit=100&brandLimit=30&group=female&ageGroup=30to34')
+            if response.status_code == 200:
+                filter_popular_data = response.json()
+                filter_popular_brand_10th = filter_popular_data['data']['popularBrandKeywords'][9]
 
-            # 필터 기준 문구 변경 확인
-            if brand_filter.text == '여성 30~34세 기준':
-                print('필터 적용 확인 - 필터 기준 문구')
-            else:
-                test_result = 'WARN'
-                warning_texts.append('필터 적용 확인 실패')
-                print(f'필터 적용 확인 실패 : {brand_filter.text}')
+                # 필터 기준 문구 변경 확인
+                if brand_filter.text == '여성 30~34세 기준':
+                    print('필터 적용 확인 - 필터 기준 문구')
+                else:
+                    test_result = 'WARN'
+                    warning_texts.append('필터 적용 확인 실패')
+                    print(f'필터 적용 확인 실패 : {brand_filter.text}')
 
-            # 변경된 기준의 인기브랜드 10위 확인
-            popular_brand_10th = popular_brand.find_element(AppiumBy.XPATH,
-                                                            '//XCUIElementTypeCell[@index="9"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]')
-            if filter_popular_brand_10th == popular_brand_10th.text:
-                print('필터 적용 확인 - 필터 기준 10위')
+                # 변경된 기준의 인기브랜드 10위 확인
+                popular_brand_10th = popular_brand.find_element(AppiumBy.XPATH,
+                                                                '//XCUIElementTypeCell[@index="9"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="1"]')
+                if filter_popular_brand_10th == popular_brand_10th.text:
+                    print('필터 적용 확인 - 필터 기준 10위')
+                else:
+                    test_result = 'WARN'
+                    warning_texts.append('필터 적용 확인 실패')
+                    print(f'필터 적용 확인 실패 : {filter_popular_brand_10th} / {popular_brand_10th.text}')
             else:
-                test_result = 'WARN'
-                warning_texts.append('필터 적용 확인 실패')
-                print(f'필터 적용 확인 실패 : {filter_popular_brand_10th} / {popular_brand_10th.text}')
+                print('인기 검색어 필터 API 호출 실패')
 
             # 필터를 전체 기준으로 재변경
             brand_filter.click()
