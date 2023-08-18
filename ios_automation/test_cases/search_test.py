6d45bc252ed6434d9c0253224ca027ce92f331e7
dajeong <dajjeong@29cm.co.kr>
2023-08-18 13:45:01 +0900

    Search의 인기검색어 시나리오 작성

--- ios_automation/test_cases/search_test.py
+++ ios_automation/test_cases/search_test.py
@@ -1,14 +1,12 @@
 import os
 import sys
 import traceback
+import requests
 
 from time import time, sleep
-
-import requests
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common import NoSuchElementException
 from com_utils import values_control
-
 import com_utils.element_control
 
 
@@ -119,6 +117,132 @@ class Search:
             wd.find_element(AppiumBy.IOS_CLASS_CHAIN,
                             f'**/XCUIElementTypeButton[`label == "{self.conf["search_filter_age"]["ALL"]}"`][2]').click()
             wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeButton[`label == "적용하기"`]').click()
+
+            # 뒤로가기
+            wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'icNavigationbarBackBlack').click()
+
+        except Exception:
+            test_result = 'FAIL'
+            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
+            img_src = os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
+            error_text = traceback.format_exc().split('\n')
+            try:
+                error_texts.append(values_control.find_next_double_value(error_text, 'Traceback'))
+                error_texts.append(values_control.find_next_value(error_text, 'Stacktrace'))
+            except Exception:
+                pass
+            wd.get('app29cm://home')
+
+        finally:
+            run_time = f"{time() - start_time:.2f}"
+            warning = [str(i) for i in warning_texts]
+            warning_points = "\n".join(warning)
+            result_data = {
+                'test_result': test_result, 'error_texts': error_texts, 'img_src': img_src,
+                'test_name': test_name, 'run_time': run_time, 'warning_texts': warning_points}
+            return result_data
+
+    def test_search_popular_keyword(self, wd, test_result='PASS', error_texts=[], img_src='', warning_texts=[]):
+        test_name = self.dconf[sys._getframe().f_code.co_name]
+        start_time = time()
+
+        try:
+            print(f'[{test_name}] 테스트 시작')
+
+            # SEARCH 탭 진입
+            wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeButton[`label == "SEARCH"`]').click()
+
+            popluar_keyword_1st = ''
+            popluar_keyword_25th = ''
+            # 전체 기준 인기검색어 리스트 호출
+            response = requests.get('https://search-api.29cm.co.kr/api/v4/keyword/popular?limit=100&brandLimit=30')
+            if response.status_code == 200:
+                all_popular_data = response.json()
+                popluar_keyword_1st = all_popular_data['data']['popularKeyword'][0]
+                popluar_keyword_25th = all_popular_data['data']['popularKeyword'][24]
+                print(f'1위: {popluar_keyword_1st} / 25위: {popluar_keyword_25th}')
+            else:
+                print('인기 검색어 API 호출 실패')
+
+            # 인기 브랜드 타이틀 확인
+            try:
+                wd.find_element(AppiumBy.ACCESSIBILITY_ID, '지금 많이 찾는 검색어')
+                print('인기 검색어 타이틀 확인')
+            except NoSuchElementException:
+                test_result = 'WARN'
+                warning_texts.append('인기 검색어 타이틀 확인 실패')
+                print('인기 검색어 타이틀 확인 실패')
+
+            # 인기 검색어 1위 검색어 선택 / 동일 검색어 2개일 경우와 1개일 경우 분리
+            try:
+                wd.find_element(AppiumBy.IOS_CLASS_CHAIN,
+                                f'**/XCUIElementTypeStaticText[`label == "{popluar_keyword_1st}"`][2]').click()
+            except NoSuchElementException:
+                wd.find_element(AppiumBy.ACCESSIBILITY_ID, popluar_keyword_1st).click()
+
+            # 연관 검색어 API 호출
+            response = requests.get(
+                f'https://search-api.29cm.co.kr/api/v4/keyword/related?keyword={popluar_keyword_1st}')
+            if response.status_code == 200:
+                relate_keyword_data = response.json()
+                relate_keyword_list = relate_keyword_data['data']['relatedKeywords']
+
+                # 연관 검색어 없을 경우, 검색 필드 확인 / 있을 경우, 첫번째 연관 검색어 확인
+                if not relate_keyword_list:
+                    print('연관 검색어 없음')
+                    search_input_field = wd.find_element(AppiumBy.CLASS_NAME, 'XCUIElementTypeTextField').text
+                    if search_input_field == popluar_keyword_1st:
+                        print('인기 검색어 검색 확인 - 입력란')
+                    else:
+                        test_result = 'WARN'
+                        warning_texts.append('인기 브랜드 검색 결과 확인 실패')
+                        print(f'인기 브랜드 검색 결과 확인 실패 : {popluar_keyword_1st} / {search_input_field}')
+                else:
+                    print('연관 검색어 있음')
+                    relate_keyword = relate_keyword_list[0]
+                    relate = wd.find_element(AppiumBy.XPATH,
+                                             '//XCUIElementTypeCollectionView[@index="1"]/XCUIElementTypeCell/XCUIElementTypeCollectionView')
+                    relate_1st = relate.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton').get_attribute('name')
+                    if relate_keyword == relate_1st:
+                        print('인기 검색어 검색 확인 - 연관 검색어')
+                    else:
+                        test_result = 'WARN'
+                        warning_texts.append('인기 브랜드 검색 결과 확인 실패')
+                        print(f'인기 브랜드 검색 결과 확인 실패 : {relate_1st} / {relate_keyword}')
+            else:
+                print('연관 검색어 API 호출 실패')
+
+            # 검색 화면으로 복귀
+            wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'icNavigationbarBackBlack').click()
+
+            # 최근 검색어에 최근에 선택한 검색어 노출 여부 확인
+            recent_keyword = wd.find_element(AppiumBy.XPATH,
+                                             '//XCUIElementTypeCollectionView[@index="1"]/XCUIElementTypeCell[@index="0"]/XCUIElementTypeOther/XCUIElementTypeStaticText[@index="0"]')
+            if popluar_keyword_1st == recent_keyword.text:
+                print('최근 검색어 노출 확인')
+            else:
+                test_result = 'WARN'
+                warning_texts.append('최근 검색어 노출 확인 실패')
+                print('최근 검색어 노출 확인 실패')
+
+            # 최근 검색어 모두 지우기
+            wd.find_element(AppiumBy.IOS_CLASS_CHAIN,
+                            '**/XCUIElementTypeStaticText[`label == "모두 지우기"`]').click()
+
+            # 인기 검색어 25위 확인
+            for i in range(0, 10):
+                try:
+                    wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeStaticText[`label == "25"`]')
+                    try:
+                        wd.find_element(AppiumBy.ACCESSIBILITY_ID, popluar_keyword_25th)
+                        print('인기 검색어 순위 확인')
+                        break
+                    except NoSuchElementException:
+                        test_result = 'WARN'
+                        warning_texts.append('인기 검색어 순위 확인 실패')
+                        print('인기 검색어 순위 확인 실패')
+                except NoSuchElementException:
+                    com_utils.element_control.scroll_control(wd, "D", 60)
 
             # 뒤로가기
             wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'icNavigationbarBackBlack').click()
