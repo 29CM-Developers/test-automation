56511966991bb96fda0a1fb482dd144cb1a9c934
dajeong <dajjeong@29cm.co.kr>
2023-08-21 16:36:29 +0900

    특정 영역 스크롤 코드 추가

--- com_utils/element_control.py
+++ com_utils/element_control.py
@@ -1,14 +1,12 @@
-import logging
+from time import sleep
 
 from appium.webdriver.common.appiumby import AppiumBy
-from appium.webdriver.common.touch_action import TouchAction
-from selenium.common import NoSuchElementException
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions import interaction
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions.pointer_input import PointerInput
 from selenium.webdriver.common.by import By
-from time import sleep
+
 
 def class_find_click(wd, class_name):
     wd.find_element(By.CLASS_NAME, f'{class_name}').click()
@@ -221,5 +219,29 @@ def scroll_control(wd, direction, percent):
     elif direction == 'D':
         start_y = size["height"] * 0.5
         end_y = (size["height"] * 0.5) - (size["height"] * 0.01 * percent)
+        wd.swipe(start_x, start_y, start_x, end_y, duration_ms)
+    sleep(2)
+
+
+def element_scroll_control(wd, element, direction, percent):
+    # 특정 요소의 위치와 사이즈 값
+    location = element.location
+    size = element.size
+
+    # 특정 요소의 중앙을 시작점으로 지정
+    start_x = location['x'] + (size['width'] / 2)
+    start_y = location['y'] + (size['height'] / 2)
+
+    # 디바이스 사이즈
+    window_size = wd.get_window_size()
+    move_y = window_size['height'] * (percent / 100)
+
+    duration_ms = 1000
+
+    if direction == 'U':
+        end_y = start_y + move_y
+        wd.swipe(start_x, start_y, start_x, end_y, duration_ms)
+    elif direction == 'D':
+        end_y = start_y - move_y
         wd.swipe(start_x, start_y, start_x, end_y, duration_ms)
     sleep(2)
