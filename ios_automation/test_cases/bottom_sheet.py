d3c76f5cf484a6b4654e7aac3d784fdd6a0e624a
dajeong <dajjeong@29cm.co.kr>
2023-08-24 17:47:49 +0900

    불필요한 코드 제거

--- ios_automation/test_cases/bottom_sheet.py
+++ ios_automation/test_cases/bottom_sheet.py
@@ -1,13 +1,8 @@
-import sys
-
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common import NoSuchElementException
 
 
 def test_bottom_sheet(wd):
-    test_name = sys._getframe().f_code.co_name
-    print(f'[{test_name}] 테스트 시작')
-
     try:
         wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeWindow[2]')
         wd.find_element(AppiumBy.ACCESSIBILITY_ID, '닫기').click()
