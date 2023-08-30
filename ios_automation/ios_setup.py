e0667e130c861f91769858beb763979983d0cd2b
mpark@29cm.co.kr <mpark@29cm.co.kr>
2023-08-30 17:44:51 +0900

    change mpark ios setup

--- ios_automation/ios_setup.py
+++ ios_automation/ios_setup.py
@@ -5,11 +5,11 @@ from appium.options.common import AppiumOptions
 def mpark_setup():
     iOS_options = AppiumOptions()
     iOS_options.set_capability("platformName", "iOS")
-    iOS_options.set_capability("platformVersion", "16.5.1")
+    iOS_options.set_capability("platformVersion", "16.0.2")
     iOS_options.set_capability("deviceName", "iPhone")
     iOS_options.set_capability("automationName", "XCUITest")
     iOS_options.set_capability("bundleId", "kr.co.29cm.App29CM")
-    iOS_options.set_capability("udid", "00008101-001230822151003A")
+    iOS_options.set_capability("udid", "00008110-001E556436D1401E")
     iOS_options.set_capability("xcodeSigningId", "iPhone Developer")
     iOS_options.set_capability("xcodeOrgId", "323Z55V788")
     iOS_options.set_capability("newCommandTimeout", 300)
