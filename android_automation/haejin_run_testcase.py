8a8ac632dd35d116b26d4e15233f101ad222edb6
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-10-11 11:16:39 +0900

    간편 회원가입 실패 케이스 추가

--- android_automation/haejin_run_testcase.py
+++ android_automation/haejin_run_testcase.py
@@ -102,6 +102,41 @@ class AndroidTestAutomation(unittest.TestCase):
         self.count = slack_result_notifications.slack_thread_notification(self)
         self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
 
+        ##########################
+        # 현재 함수명 저장 - slack noti에 사용
+        self.def_name = self.dconf[sys._getframe().f_code.co_name]
+        # 노트20 시나리오
+        # 실제 실행 - 이메일 로그인 성공
+        self.result_data = LoginLogout.test_email_login_success(self, self.wd)
+        self.response = slack_result_notifications.slack_notification(self)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        # 실제 실행 - 홈 화면에서 다른 탭으로 이동 성공
+        self.result_data = Home.test_move_tab_from_home(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        # 실제 실행 - 홈 배너 성공
+        self.result_data = Home.test_home_banner(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        # 실제 실행 - 홈 컨텐츠 성공
+        self.result_data = Home.test_home_contents(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        # 실제 실행 -   카테고리 기능 확인 성공
+        self.result_data = Category.test_category_page(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
+        # 실제 실행 -   LIKE 존재하지 않는 경우 화면 확인 성공
+        self.result_data = Like.test_no_like_item(self, self.wd)
+        self.count = slack_result_notifications.slack_thread_notification(self)
+        self.total_time, self.slack_result = slack_result_notifications.slack_update_notification(self)
+
         # 실제 실행 -   LIKE 존재하는 경우 화면 확인 성공
         self.result_data = Like.test_like_item(self, self.wd)
         self.count = slack_result_notifications.slack_thread_notification(self)
