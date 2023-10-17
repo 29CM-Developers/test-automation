93da5d3dc8f88489e3da7e18e712f4045a2262ce
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-10-17 13:14:14 +0900

    주문 건이 없을 경우, 주문 배송 조회 시나리오

--- android_automation/test_cases/my_test.py
+++ android_automation/test_cases/my_test.py
@@ -201,3 +201,68 @@ class My:
                 'test_result': test_result, 'error_texts': error_texts, 'img_src': img_src,
                 'test_name': test_name, 'run_time': run_time, 'warning_texts': warning_points}
             return result_data
+
+    def test_track_delivery_without_orders(self, wd, test_result='PASS', error_texts=[], img_src='',
+                                           warning_texts=[]):
+        # 현재 함수명 저장 - slack noti에 사용
+        test_name = self.dconf[sys._getframe().f_code.co_name]
+        # slack noti에 사용하는 테스트 소요시간을 위해 함수 시작 시 시간 체크
+        start_time = time()
+        try:
+            print("[주문 건이 없을 경우, 주문 배송 조회 확인] CASE 시작")
+            # 하단 네비게이터에 MY 메뉴 진입
+            sleep(2)
+            wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'MY').click()
+            sleep(3)
+            print("홈 > 마이페이지 화면 진입")
+            try:
+                wd.find_element(By.XPATH, "//*[contains(@text, '주문배송조회')]").click()
+                sleep(2)
+                order_delivery_tracking_title = wd.find_elements(By.XPATH, "//*[contains(@text, '주문배송조회')]")
+                order_delivery_tracking_guide = wd.find_element(By.XPATH, "//*[contains(@text, '주문내역이 없습니다')]")
+                print(f"order_delivery_tracking_title.text : {order_delivery_tracking_title[1].text}")
+                print(f"order_delivery_tracking_guide.text : {order_delivery_tracking_guide.text}")
+                if order_delivery_tracking_title[
+                    1].text == '주문배송조회' and order_delivery_tracking_guide.text == '주문내역이 없습니다':
+                    print("주문 건이 없을 경우, 주문 배송 조회 확인")
+                else:
+                    print("주문 건이 없을 경우, 주문 배송 조회 확인 실패")
+                    test_result = 'WARN'
+                    warning_texts.append("주문 건이 없을 경우, 주문 배송 조회 확인 실패")
+                # 뒤로가기로 마이페이지 진입 확인
+                wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/imgBack').click()
+
+            except NoSuchElementException:
+                print("NoSuchElementException 주문 건이 없을 경우, 주문 배송 조회 확인 실패")
+                sleep(1)
+            wd.get('app29cm://home')
+            print("[주문 건이 없을 경우, 주문 배송 조회 확인] CASE 종료")
+
+        except Exception:
+            # 오류 발생 시 테스트 결과를 실패로 한다
+            test_result = 'FAIL'
+            # 스크린샷
+            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
+            # 스크린샷 경로 추출
+            img_src = os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
+            # 에러 메시지 추출
+            error_text = traceback.format_exc().split('\n')
+            try:
+                # 에러메시지 분류 시 예외처리
+                error_texts.append(values_control.find_next_double_value(error_text, 'Traceback'))
+                error_texts.append(values_control.find_next_value(error_text, 'Stacktrace'))
+            except Exception:
+                pass
+            wd.get('app29cm://home')
+
+        finally:
+            # 함수 완료 시 시간체크하여 시작시 체크한 시간과의 차이를 테스트 소요시간으로 반환
+            run_time = f"{time() - start_time:.2f}"
+            # warning texts list를 가독성 좋도록 줄바꿈
+            warning = [str(i) for i in warning_texts]
+            warning_points = "\n".join(warning)
+            # 값 재사용 용이성을 위해 dict로 반환한다
+            result_data = {
+                'test_result': test_result, 'error_texts': error_texts, 'img_src': img_src,
+                'test_name': test_name, 'run_time': run_time, 'warning_texts': warning_points}
+            return result_data
