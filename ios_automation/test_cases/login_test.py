5e77fa1c4bd6a0fd8195fca24f00b2a742ec9503
dajeong <dajjeong@29cm.co.kr>
2023-10-24 19:23:12 +0900

    로그인 테스트 스크립트 구조화

--- ios_automation/test_cases/login_test.py
+++ ios_automation/test_cases/login_test.py
@@ -1,80 +1,16 @@
 import os.path
 import sys
 import traceback
+import com_utils.deeplink_control
 
-from time import sleep, time
-from appium.webdriver.common.appiumby import AppiumBy
-from selenium.common.exceptions import NoSuchElementException
+from time import time
 from com_utils import values_control
+from ios_automation.page_action import login_page, my_page, navigation_bar
 from ios_automation.test_cases.bottom_sheet import test_bottom_sheet
 from ios_automation.test_cases.select_category_page import test_select_category
 
 
 class UserLoginTest:
-
-    def input_id_password(self, wd, id, password):
-
-        wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeOther[@name="로그인 - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeTextField[@index="0"]').send_keys(id)
-        wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeOther[@name="로그인 - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeSecureTextField[@index="1"]').send_keys(password)
-        wd.find_element(AppiumBy.ACCESSIBILITY_ID, '로그인하기').click()
-
-    def test_email_login_error(self, wd, test_result='PASS', error_texts=[], img_src='', warning_texts=[]):
-        test_name = self.dconf[sys._getframe().f_code.co_name]
-        start_time = time()
-
-        try:
-            print(f'[{test_name}] 테스트 시작')
-
-            # 로그인 페이지 진입
-            wd.get(self.conf['deeplink']['my'])
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="로그인·회원가입"]').click()
-            sleep(3)
-
-            # 올바른 이메일, 잘못된 비밀번호 입력하여 에러 문구 확인
-            UserLoginTest.input_id_password(self, wd, self.pconf['id_29cm'], self.pconf['error_password_29cm'])
-            sleep(3)
-            error = wd.find_element(AppiumBy.XPATH,
-                                    '//XCUIElementTypeOther[@name="로그인 - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText').text
-
-            if self.conf['login_error_text'] in error:
-                print("이메일 로그인 실패 확인")
-            elif self.conf['login_exceeded_text'] in error:
-                print("이메일 로그인 실패 확인")
-            else:
-                test_result = 'WARN'
-                warning_texts.append('이메일 로그인 실패 확인 실패')
-                print("이메일 로그인 실패 확인 실패")
-
-            # Home으로 복귀
-            wd.find_element(AppiumBy.ACCESSIBILITY_ID, 'common back icon black').click()
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="HOME"]').click()
-
-        except Exception:
-            # 오류 발생 시 테스트 결과를 실패로 한다
-            test_result = 'FAIL'
-            # 스크린샷
-            wd.get_screenshot_as_file(sys._getframe().f_code.co_name + '_error.png')
-            # 스크린샷 경로 추출
-            img_src = os.path.abspath(sys._getframe().f_code.co_name + '_error.png')
-            # 에러 메시지 추출
-            error_text = traceback.format_exc().split('\n')
-            try:
-                # 에러메시지 분류 시 예외처리
-                error_texts.append(values_control.find_next_double_value(error_text, 'Traceback'))
-                error_texts.append(values_control.find_next_value(error_text, 'Stacktrace'))
-            except Exception:
-                pass
-            wd.get(self.conf['deeplink']['home'])
-
-        finally:
-            run_time = f"{time() - start_time:.2f}"
-            warning = [str(i) for i in warning_texts]
-            warning_points = "\n".join(warning)
-            result_data = {
-                'test_result': test_result, 'error_texts': error_texts, 'img_src': img_src,
-                'test_name': test_name, 'run_time': run_time, 'warning_texts': warning_points}
-            return result_data
-
     def test_email_login_success(self, wd, test_result='PASS', error_texts=[], img_src='', warning_texts=[]):
         test_name = self.dconf[sys._getframe().f_code.co_name]
         start_time = time()
@@ -83,29 +19,21 @@ class UserLoginTest:
             print(f'[{test_name}] 테스트 시작')
 
             # 로그인 페이지 진입
-            wd.get(self.conf['deeplink']['my'])
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="로그인·회원가입"]').click()
-            sleep(3)
+            com_utils.deeplink_control.move_to_my(self, wd)
+            my_page.enter_login_page(wd)
 
             # 올바른 아이디, 올바른 비밀번호 입력
-            UserLoginTest.input_id_password(self, wd, self.pconf['id_29cm'], self.pconf['password_29cm'])
-            sleep(3)
+            login_page.input_id_password(wd, self.pconf['id_29cm'], self.pconf['password_29cm'])
 
             # 프로필 이름 확인
-            try:
-                wd.find_element(AppiumBy.ACCESSIBILITY_ID, self.pconf['nickname'])
-                print("이메일 로그인 성공 확인")
-            except:
-                test_result = 'WARN'
-                warning_texts.append('이메일 로그인 성공 확인 실패')
-                print("이메일 로그인 성공 확인 실패")
+            test_result = my_page.check_nickname(self, wd, warning_texts)
 
             # Home 으로 복귀
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="HOME"]').click()
+            navigation_bar.move_to_home(wd)
 
             # 복귀 후, 홈 탭 진입 전 노출 화면 있는지 확인
-            test_select_category(self.wd)
-            test_bottom_sheet(self.wd)
+            test_select_category(wd)
+            test_bottom_sheet(wd)
 
         except Exception:
             test_result = 'FAIL'
@@ -117,7 +45,7 @@ class UserLoginTest:
                 error_texts.append(values_control.find_next_value(error_text, 'Stacktrace'))
             except Exception:
                 pass
-            wd.get(self.conf['deeplink']['home'])
+            com_utils.deeplink_control.move_to_home(self, wd)
 
         finally:
             run_time = f"{time() - start_time:.2f}"
@@ -136,40 +64,18 @@ class UserLoginTest:
             print(f'[{test_name}] 테스트 시작')
 
             # My 탭 딥링크로 진입
-            wd.get(self.conf['deeplink']['my'])
-            try:
-                wd.find_element(AppiumBy.ACCESSIBILITY_ID, self.pconf['nickname'])
-                print("로그인 유저")
-            except:
-                pass
-                print("비로그인 유저")
+            com_utils.deeplink_control.move_to_my(self, wd)
 
             # 로그아웃 버튼 선택
-            for i in range(0, 5):
-                try:
-                    logout_btn = wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="LOGOUT"]')
-                    logout_btn.click()
-                    break
-                except NoSuchElementException:
-                    wd.execute_script('mobile:swipe', {'direction': 'up'})
+            my_page.find_logout_btn(wd)
+            my_page.click_logout_btn(wd)
 
-            # 로그아웃 완료 후 문구 확인
-            for i in range(0, 5):
-                try:
-                    wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="로그인·회원가입"]')
-                    break
-                except NoSuchElementException:
-                    wd.execute_script('mobile:swipe', {'direction': 'down'})
-            try:
-                wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeStaticText[@name="로그인·회원가입"]')
-                print('로그아웃 성공 확인')
-            except NoSuchElementException:
-                test_result = 'WARN'
-                warning_texts.append('로그아웃 성공 확인 실패')
-                print('로그아웃 성공 확인')
+            # 로그아웃 완료 > 로그인,회원가입 문구 확인
+            my_page.find_login_btn(wd)
+            test_result = my_page.check_login_btn(wd, warning_texts)
 
             # Home 으로 복귀
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="HOME"]').click()
+            navigation_bar.move_to_home(wd)
 
         except Exception:
             test_result = 'FAIL'
@@ -181,7 +87,7 @@ class UserLoginTest:
                 error_texts.append(values_control.find_next_value(error_text, 'Stacktrace'))
             except Exception:
                 pass
-            wd.get(self.conf['deeplink']['home'])
+            com_utils.deeplink_control.move_to_home(self, wd)
 
         finally:
             run_time = f"{time() - start_time:.2f}"
@@ -200,50 +106,28 @@ class UserLoginTest:
             print(f'[{test_name}] 테스트 시작')
 
             # 로그인 페이지 진입
-            wd.get(self.conf['deeplink']['my'])
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="로그인·회원가입"]').click()
-            sleep(3)
+            com_utils.deeplink_control.move_to_my(self, wd)
+            my_page.enter_login_page(wd)
 
             # 올바른 이메일, 잘못된 비밀번호 입력하여 에러 문구 확인
-            UserLoginTest.input_id_password(self, wd, self.pconf['id_29cm'], self.pconf['error_password_29cm'])
-            sleep(3)
-            error = wd.find_element(AppiumBy.XPATH,
-                                    '//XCUIElementTypeOther[@name="로그인 - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeOther[1]/XCUIElementTypeStaticText').text
-
-            if self.conf['login_error_text'] in error:
-                print("이메일 로그인 실패 확인")
-            elif self.conf['login_exceeded_text'] in error:
-                print("이메일 로그인 실패 확인")
-            else:
-                test_result = 'WARN'
-                warning_texts.append('이메일 로그인 실패 확인 실패')
-                print("이메일 로그인 실패 확인 실패")
+            login_page.input_id_password(wd, self.pconf['id_29cm'], self.pconf['error_password_29cm'])
+            test_result = login_page.check_login_error_text(self, wd, warning_texts)
 
             # 이메일, 비밀번호 입력값 제거
-            wd.find_element(AppiumBy.XPATH,
-                            '//XCUIElementTypeOther[@name="로그인 - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeTextField[@index="0"]').clear()
-            wd.find_element(AppiumBy.XPATH,
-                            '//XCUIElementTypeOther[@name="로그인 - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeSecureTextField[@index="1"]').clear()
+            login_page.clear_id_password(wd)
 
             # 올바른 아이디, 올바른 비밀번호 입력
-            UserLoginTest.input_id_password(self, wd, self.pconf['id2_29cm'], self.pconf['password_29cm'])
-            sleep(3)
+            login_page.input_id_password(wd, self.pconf['id2_29cm'], self.pconf['password_29cm'])
 
             # 프로필 이름 확인
-            try:
-                wd.find_element(AppiumBy.ACCESSIBILITY_ID, self.pconf['nickname'])
-                print("이메일 로그인 성공 확인")
-            except NoSuchElementException:
-                test_result = 'WARN'
-                warning_texts.append('이메일 로그인 성공 확인 실패')
-                print("이메일 로그인 성공 확인 실패")
+            test_result = my_page.check_nickname(self, wd, warning_texts)
 
             # Home 으로 복귀
-            wd.find_element(AppiumBy.XPATH, '//XCUIElementTypeButton[@name="HOME"]').click()
+            navigation_bar.move_to_home(wd)
 
             # 복귀 후, 홈 탭 진입 전 노출 화면 있는지 확인
-            test_select_category(self.wd)
-            test_bottom_sheet(self.wd)
+            test_select_category(wd)
+            test_bottom_sheet(wd)
 
         except Exception:
             test_result = 'FAIL'
@@ -255,7 +139,7 @@ class UserLoginTest:
                 error_texts.append(values_control.find_next_value(error_text, 'Stacktrace'))
             except Exception:
                 pass
-            wd.get(self.conf['deeplink']['home'])
+            com_utils.deeplink_control.move_to_home(self, wd)
 
         finally:
             run_time = f"{time() - start_time:.2f}"
