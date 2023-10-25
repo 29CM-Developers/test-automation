84f241793e6f569eb24376699ae45742a5d166cd
dajeong <dajjeong@29cm.co.kr>
2023-10-25 19:30:14 +0900

    딥링크 추가

--- com_utils/deeplink_control.py
+++ com_utils/deeplink_control.py
@@ -1,3 +1,6 @@
+from time import sleep
+
+
 # Home 탭으로 이동 딥링크
 def move_to_home(self, wd):
     wd.get(self.conf['deeplink']['home'])
@@ -13,3 +16,12 @@ def move_to_like(self, wd):
 
 def move_to_my(self, wd):
     wd.get(self.conf['deeplink']['my'])
+
+
+def move_to_welove(self, wd):
+    wd.get(self.conf['deeplink']['welove'])
+
+
+def move_to_pdp(wd, product_item_no):
+    wd.get(f'app29cm://product/{product_item_no}')
+    sleep(3)
