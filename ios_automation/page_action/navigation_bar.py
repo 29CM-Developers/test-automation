1c99f98f1832c89e591fb54ff82464978e27a1da
dajeong <dajjeong@29cm.co.kr>
2023-11-06 08:29:58 +0900

    홈 탭 이동 시, 바텀시트 확인 코드 추가

--- ios_automation/page_action/navigation_bar.py
+++ ios_automation/page_action/navigation_bar.py
@@ -1,5 +1,7 @@
 from appium.webdriver.common.appiumby import AppiumBy
+from ios_automation.page_action.bottom_sheet import close_bottom_sheet
 
 
 def move_to_home(wd):
     wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeButton[`label == "HOME"`]').click()
+    close_bottom_sheet(wd)
