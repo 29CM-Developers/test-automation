f9d5c2962253fabb34086584636ef5240b35f675
dajeong <dajjeong@29cm.co.kr>
2023-11-08 17:22:12 +0900

    GNB로 탭 이동 코드 작성

--- ios_automation/page_action/navigation_bar.py
+++ ios_automation/page_action/navigation_bar.py
@@ -1,7 +1,24 @@
 from appium.webdriver.common.appiumby import AppiumBy
 from ios_automation.page_action.bottom_sheet import close_bottom_sheet
+from com_utils.element_control import ialc
 
 
 def move_to_home(wd):
-    wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeButton[`label == "HOME"`]').click()
+    ialc(wd, '**/XCUIElementTypeButton[`label == "HOME"`]')
     close_bottom_sheet(wd)
+
+
+def move_to_category(wd):
+    ialc(wd, '**/XCUIElementTypeButton[`label == "CATEGORY"`]')
+
+
+def move_to_search(wd):
+    ialc(wd, '**/XCUIElementTypeButton[`label == "SEARCH"`]')
+
+
+def move_to_like(wd):
+    ialc(wd, '**/XCUIElementTypeButton[`label == "LIKE"`]')
+
+
+def move_to_my(wd):
+    ialc(wd, '**/XCUIElementTypeButton[`label == "MY"`]')
