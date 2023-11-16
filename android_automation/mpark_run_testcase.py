d6639e05dc86c91c99d20c59f7bf26404016d394
hyunjunpark-29cm <mpark@29cm.co.kr>
2023-11-16 09:55:05 +0900

    testrail API 사용법 예제

--- android_automation/mpark_run_testcase.py
+++ android_automation/mpark_run_testcase.py
@@ -8,18 +8,34 @@ import requests
 import subprocess
 
 from appium.webdriver.appium_service import AppiumService
+from appium.webdriver.common.appiumby import AppiumBy
 from android_setup import mpark_setup
+from time import sleep
 from android_automation.test_cases.sample_test import AutomationTesting
 from com_utils import values_control, slack_result_notifications
+from com_utils.element_control import aal, aalc, aalk, aals
+from com_utils.testrail_api import *
 
 
 class AndroidTestAutomation(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.pconf = requests.get(f"http://192.168.103.13:50/qa/personal/mpark").json()
+        cls.conf = requests.get(f"http://192.168.103.13:50/qa/personal/info").json()
+        cls.dconf = requests.get(f"http://192.168.103.13:50/qa/personal/def_names").json()
+        cls.econf = requests.get(f"http://192.168.103.13:50/qa/personal/test_environment").json()
+
+        # report data
+        cls.count = 0
+        cls.result_lists = []
+        cls.total_time = ''
+        cls.slack_result = ''
+
+        cls.testcase_data = create_plan(cls, 'iOS')
+        cls.testcases = get_tests(cls)
+
 
     def setUp(self):
-        self.pconf = requests.get(f"http://192.168.103.13:50/qa/personal/mpark").json()
-        self.conf = requests.get(f"http://192.168.103.13:50/qa/personal/info").json()
-        self.dconf = requests.get(f"http://192.168.103.13:50/qa/personal/def_names").json()
-
         # Appium Service
         self.appium = AppiumService()
         self.appium.start(args=['-p', '4723', '--base-path', '/wd/hub', '--default-capabilities',
@@ -28,14 +44,12 @@ class AndroidTestAutomation(unittest.TestCase):
         # webdriver
         self.wd, self.and_cap = mpark_setup()
         self.wd.implicitly_wait(5)
-
-        # report data
-        self.count = 0
-        self.result_lists = []
-        self.total_time = ''
-        self.slack_result = ''
         self.device_platform = self.and_cap.capabilities['platformName']
         self.device_name = self.and_cap.capabilities['appium:deviceName']
+
+    @classmethod
+    def tearDownClass(cls):
+        close_plan(cls)
 
     def tearDown(self):
         try:
@@ -63,8 +77,26 @@ class AndroidTestAutomation(unittest.TestCase):
 
 
     def test_pass(self):
+        # start_time = time.time()
+        # self.wd.find_element(AppiumBy.ACCESSIBILITY_ID, "women_tab").click()
+        sleep(10)
+        post_layer = aal(self.wd, 'com.the29cm.app29cm:id/weloveRecyclerView')
+
+        post = aal(post_layer, '//android.widget.LinearLayout[3]/android.widget.RelativeLayout/android.widget.TextView[1]')
+
+        if post.is_displayed():
+            print('display success')
+        else:
+            print('display fail')
+        # post = post_layer.find_element(AppiumBy.XPATH, '//android.widget.LinearLayout[3]/android.widget.RelativeLayout/android.widget.TextView[1]')
+        # print(f"{time.time() - start_time:.2f}")
+        sleep(2)
+
+    def test_testrail_api(self):
         pass
 
+    def test_pass(self):
+        send_test_result(self, 'PASS', '설정화면 진입')
 
 if __name__ == '__main__':
     unittest.main()
