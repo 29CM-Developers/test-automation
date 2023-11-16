5ab4fefce6155dcc399045b7fd00cd8246437225
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-11-16 18:18:44 +0900

    welove page 미사용 기능 제거

--- android_automation/page_action/welove_page.py
+++ android_automation/page_action/welove_page.py
@@ -33,20 +33,6 @@ def save_first_post_title(wd):
     return post_title
 
 
-def click_first_post(wd):
-    wd.find_element(AppiumBy.IOS_CLASS_CHAIN, '**/XCUIElementTypeCell[`name == "recommended_post"`][1]').click()
-    sleep(3)
-
-
-# like_post_name = 포스트 제목
-def click_post_like_btn(wd, like_post_name):
-    like_post_name = ' '.join(like_post_name.split())
-    post_view = wd.find_elements(AppiumBy.XPATH,
-                                 f'//XCUIElementTypeOther[@name="{like_post_name} - 감도 깊은 취향 셀렉트샵 29CM"]/XCUIElementTypeOther')
-    post_view_len = len(post_view) - 1
-    wd.find_element(AppiumBy.XPATH, f'//XCUIElementTypeOther[{post_view_len}]/XCUIElementTypeButton[1]').click()
-
-
 def save_first_post_hashtag(wd):
     post_hash_tag = ''
     for i in range(0, 3):
