4f94e4da90460cc20cbfdc076ada35433a7bffdd
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-11-16 16:41:06 +0900

    element_control 호출 부분 변경

--- android_automation/test_cases/category_test.py
+++ android_automation/test_cases/category_test.py
@@ -6,8 +6,13 @@ import requests
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
-from com_utils import values_control, element_control, api_control
+from android_automation.page_action.bottom_sheet import close_bottom_sheet
+from com_utils import values_control, api_control, deeplink_control
 from time import sleep, time
+from com_utils.element_control import aal, aalk, aalc, scroll_to_element_id, scroll_up_to_element_id, scroll_control, \
+    swipe_control, scroll_to_element_with_text, scroll, swipe_control, element_scroll_control
+from android_automation.page_action import category_page, welove_page, navigation_bar
+
 logger = logging.getLogger(name='Log')
 logger.setLevel(logging.INFO)  ## 경고 수준 설정
 formatter = logging.Formatter('|%(name)s||%(lineno)s|%(message)s', datefmt='%Y-%m-%d %H:%M:%S')
@@ -69,7 +74,7 @@ class Category:
                 products = api_data['data']['products']
                 first_product_name = products[0]['itemName']
                 print(f"first_product_name : {first_product_name}")
-                element = element_control.scroll_to_element_with_text(wd, first_product_name)
+                element = scroll_to_element_with_text(wd, first_product_name)
                 if element.text in first_product_name:
                     print('상품 목록의 1번째 아이템명 일치 확인')
                 else :
@@ -78,14 +83,14 @@ class Category:
                     print('상품 목록의 1번째 아이템명 일치 확인 실패')
             else:
                 print('카테고리 그룹 API 호출 실패')
-            element = element_control.scroll_up_to_element_id(wd, '샌들')
+            element = scroll_up_to_element_id(wd, '샌들')
             element.click()
             sleep(3)
             try:
                 page_title = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/mediumCategory')
                 print(f'메뉴 트리 노출 확인 : {page_title.text}')
                 if '샌들' in page_title.text:
-                    print('중 카테고리 페이지 진입 확인 확인')
+                    print('중 카테고리 페이지 진입 확인')
                 else:
                     test_result = 'WARN'
                     warning_texts.append('중 카테고리 페이지 진입 확인 실패')
@@ -95,7 +100,8 @@ class Category:
                 warning_texts.append('중 카테고리 페이지 진입 확인 실패')
                 print('WARN : 중 카테고리 페이지 진입 확인 실패')
             selector_layer = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/selector')
-            selector = selector_layer.find_element(AppiumBy.XPATH, '//android.view.View/android.view.View/android.view.View[1]/android.widget.TextView')
+            selector = aal(selector_layer,
+                           '//android.view.View/android.view.View/android.view.View[2]/android.widget.TextView')
             # 필터링 버튼 선택
             selector.click()
             buttom_layer = wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/design_bottom_sheet')
@@ -115,7 +121,7 @@ class Category:
                 products = api_data['data']['products']
                 first_product_name = products[0]['itemName']
                 print(f"first_product_name : {first_product_name}")
-                element = element_control.scroll_to_element_with_text(wd, first_product_name)
+                element = scroll_to_element_with_text(wd, first_product_name)
                 if element.text in first_product_name :
                     print('상품 목록의 1번째 아이템명 일치 확인')
                 else :
