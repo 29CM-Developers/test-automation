0d80fba99728270e18ef8f58024c0f30ed6d21e9
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-11-16 23:37:13 +0900

    like 화면에서 바텀시트 발생 제거 시나리오 추가

--- android_automation/page_action/bottom_sheet.py
+++ android_automation/page_action/bottom_sheet.py
@@ -16,9 +16,17 @@ def close_bottom_sheet(wd):
 
 def close_like_bottom_sheet(wd):
     try:
-        aal(wd, 'com.the29cm.app29cm:id/com_braze_inappmessage_html')
-        aalc(wd, '다음에')
-        print('바텀 시트 노출되어 닫기 동작')
+        braze = aal(wd, 'com.the29cm.app29cm:id/com_braze_inappmessage_html')
+        if braze == None:
+            print('바텀 시트 미노출')
+        else:
+            like_bottom_sheet = aal(wd, '다음에')
+            if like_bottom_sheet == None:
+                print('바텀 시트 미노출')
+                pass
+            else:
+                aalc(wd, '다음에')
+                print('바텀 시트 노출되어 닫기 동작')
 
     except NoSuchElementException:
         print('바텀 시트 미노출')
