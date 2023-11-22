f3bf0d3d5327be647c78c04fe2c9f74142b72078
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-11-22 15:06:31 +0900

    plp 보강시나리오 추가

---
+++ android_automation/page_action/best_product_list_page.py
@@ -0,0 +1,31 @@
+from time import sleep
+from appium.webdriver.common.appiumby import AppiumBy
+from selenium.common import NoSuchElementException
+from com_utils.element_control import aal, aalc
+from com_utils.element_control import scroll_control
+
+
+def click_back_btn(wd):
+    aalc(wd, 'com.the29cm.app29cm:id/imgBack')
+
+
+# period = 실시간, 일간, 주간, 월간
+def click_period_sort(wd, period):
+    aalc(wd, f'c_{period}')
+
+
+def save_best_first_product_name(wd):
+    product_name = aal(wd, 'best_item_title').text
+    return product_name
+
+
+def check_best_product_name(warning_texts, compare_name, product_name):
+    print(f'베스트 PLP 상품명 확인 : {compare_name} / {product_name}')
+    if compare_name in product_name:
+        test_result = 'PASS'
+        print('베스트 PLP 상품명 확인')
+    else:
+        test_result = 'WARN'
+        warning_texts.append('베스트 PLP 상품명 확인 실패')
+        print(f'베스트 PLP 상품명 확인 실패 : {compare_name} / {product_name}')
+    return test_result
