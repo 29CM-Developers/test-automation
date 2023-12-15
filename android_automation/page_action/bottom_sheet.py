1862a1ab1e06743cc0915be5ddfcdc44c2836add
haejinhong-29cm <hhj2008@29cm.co.kr>
2023-12-15 20:14:53 +0900

    바텀시트 발생 추가

--- android_automation/page_action/bottom_sheet.py
+++ android_automation/page_action/bottom_sheet.py
@@ -15,6 +15,21 @@ def close_bottom_sheet(wd):
         pass
 
 
+def close_pdp_bottom_sheet(wd):
+    try:
+        bottom_sheet = aal(wd, 'c_유용한 기능을 소개해요')
+        if bottom_sheet == None:
+            print('바텀 시트 미노출')
+        else:
+            tap_control(wd)
+            print('유용한 기능을 소개해요 바텀 시트 노출되어 닫기 동작')
+
+    except NoSuchElementException:
+        print('바텀 시트 미노출')
+        pass
+
+
+
 def close_like_bottom_sheet(wd):
     try:
         sleep(2)
