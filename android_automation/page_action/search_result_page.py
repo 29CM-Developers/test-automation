befd09abbb0d61235c0ba986d2549d793b02b803
haejinhong-29cm <hhj2008@29cm.co.kr>
2024-02-29 14:10:17 +0900

    간헐적 에러 슬립추가

--- android_automation/page_action/search_result_page.py
+++ android_automation/page_action/search_result_page.py
@@ -39,6 +39,7 @@ def check_product_brand_name(wd, compare_brand_name):
 
 
 def check_search_product_name(wd, compare_name):
+    sleep(1)
     product_name = ''
     for i in range(0, 3):
         try:
@@ -143,6 +144,7 @@ def click_color_filter(self, wd, color):
 
 
 def click_category_filter(wd, category):
+    sleep(1)
     aalc(wd, 'category_filter_layer')
     aalc(wd, f"c_{category}")
     print(f'필터 - 카테고리 : {category} 선택')
