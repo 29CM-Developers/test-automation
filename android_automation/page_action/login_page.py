133fbe2137e0edc3e4bf9bcb648547c1d5660fd5
haejinhong-29cm <hhj2008@29cm.co.kr>
2024-03-04 12:57:33 +0900

    로그인 화면 체크 수정

--- android_automation/page_action/login_page.py
+++ android_automation/page_action/login_page.py
@@ -31,8 +31,10 @@ def check_login_error_text(self, wd):
 
 
 def check_login_page(wd):
+    sleep(3)
     # 로그인 화면 진입 확인
-    login_page_title = aal(wd, '//*[@resource-id="__next"]/android.widget.TextView[1]')
+    login_page_title = aal(wd, '//*[@resource-id="__next"]/android.view.View[1]/android.widget.Button')
+    print(f'login_page_title :{login_page_title.text}')
     print("홈 > 마이페이지 > 로그인 화면 진입")
     if '로그인' in login_page_title.text:
         print("로그인 문구 확인")
