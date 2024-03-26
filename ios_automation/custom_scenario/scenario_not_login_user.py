f879cc2d2f479b85f6f377f1cc8ef19b1527743c
dajeong <dajjeong@29cm.co.kr>
2024-03-26 10:23:37 +0900

    파이프라인봇 실험 위한 코드 수정

--- ios_automation/custom_scenario/scenario_not_login_user.py
+++ ios_automation/custom_scenario/scenario_not_login_user.py
@@ -42,7 +42,7 @@ class IOSTestAutomation(unittest.TestCase):
 
         self.device_platform = self.iOS_cap.capabilities['platformName']
         self.device_name = self.iOS_cap.capabilities['appium:deviceName']
-        self.user = 'pipeline'
+        self.user = 'local'
 
         context = self.wd.contexts
         print(f'context 최초 확인 : {context}')
