eb0f01e03c2753d19db7b574477c99d5e3d247f7
haejinhong-29cm <hhj2008@29cm.co.kr>
2024-03-27 20:02:48 +0900

    커스텀 시나리오 분기처리

--- android_automation/custom_scenario/scenario_category.py
+++ android_automation/custom_scenario/scenario_category.py
@@ -23,7 +23,7 @@ class AndroidTestAutomation(unittest.TestCase):
         cls.count = 0
         cls.total_time = ''
         cls.slack_result = ''
-        cls.user = 'manual'
+        cls.user = 'custom'
 
     def setUp(self):
 
