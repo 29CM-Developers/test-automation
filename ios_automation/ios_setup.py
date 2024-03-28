e8c5ae4c44cae49202d214f89cba1baa6e550191
dajeong <dajjeong@29cm.co.kr>
2024-03-28 10:51:41 +0900

    테스트 아이폰 추가 및 업데이트 정보 수정

--- ios_automation/ios_setup.py
+++ ios_automation/ios_setup.py
@@ -89,7 +89,7 @@ def pro14_setup():
 def pro12_setup():
     iOS_options = AppiumOptions()
     iOS_options.set_capability("platformName", "iOS")
-    iOS_options.set_capability("platformVersion", "16.5.1")
+    iOS_options.set_capability("platformVersion", "17.4.1")
     iOS_options.set_capability("deviceName", "iPhone12pro")
     iOS_options.set_capability("automationName", "XCUITest")
     iOS_options.set_capability("wdaLocalPort", "8102")
@@ -104,5 +104,27 @@ def pro12_setup():
     iOS_options.set_capability('autoAcceptAlerts', True)
 
     wd = webdriver.Remote('http://0.0.0.0:4744/wd/hub', options=iOS_options)
+
+    return wd, iOS_options
+
+
+def iphone14_setup():
+    iOS_options = AppiumOptions()
+    iOS_options.set_capability("platformName", "iOS")
+    iOS_options.set_capability("platformVersion", "17.2.1")
+    iOS_options.set_capability("deviceName", "iPhone14")
+    iOS_options.set_capability("automationName", "XCUITest")
+    iOS_options.set_capability("wdaLocalPort", "8103")
+    iOS_options.set_capability("bundleId", "kr.co.29cm.App29CM")
+    iOS_options.set_capability("udid", "00008110-001E556436D1401E")
+    iOS_options.set_capability("xcodeSigningId", "iPhone Developer")
+    iOS_options.set_capability("xcodeOrgId", "323Z55V788")
+    iOS_options.set_capability("newCommandTimeout", 300)
+    iOS_options.set_capability("noReset", True)
+    iOS_options.set_capability("simpleIsVisibleCheck", True)
+    iOS_options.set_capability("useJSONSource", True)
+    iOS_options.set_capability('autoAcceptAlerts', True)
+
+    wd = webdriver.Remote('http://0.0.0.0:4745/wd/hub', options=iOS_options)
 
     return wd, iOS_options
