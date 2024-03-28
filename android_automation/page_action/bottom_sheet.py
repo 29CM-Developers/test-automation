8013ac0ef599328da5ef08d61d4833d9b33f6b4d
haejinhong-29cm <hhj2008@29cm.co.kr>
2024-03-28 17:02:03 +0900

    바텀시트 발생 시나리오 수정

--- android_automation/page_action/bottom_sheet.py
+++ android_automation/page_action/bottom_sheet.py
@@ -2,15 +2,18 @@ from time import sleep
 
 from appium.webdriver.common.appiumby import AppiumBy
 from selenium.common import NoSuchElementException
+from android_automation.page_action import selection_page
 from com_utils.element_control import aal, aalc, tap_control
 
 
 def close_bottom_sheet(wd):
     try:
-        wd.find_element(AppiumBy.ID, 'com.the29cm.app29cm:id/com_braze_inappmessage_html')
-        wd.find_element(AppiumBy.XPATH, "//*[contains(@text, '닫기')]").click()
-        print('바텀 시트 노출되어 닫기 동작')
-
+        braze = aal(wd, 'com.the29cm.app29cm:id/com_braze_inappmessage_html')
+        if braze == None :
+            print('바텀 시트 미노출')
+        else :
+            aalc(braze, 'c_닫기')
+            print('바텀 시트 노출되어 닫기 동작')
     except NoSuchElementException:
         print('바텀 시트 미노출')
         pass
@@ -24,6 +27,7 @@ def close_pdp_bottom_sheet(wd):
         else:
             tap_control(wd)
             print('유용한 기능을 소개해요 바텀 시트 노출되어 닫기 동작')
+        selection_page.click_close_selection_pop_up(wd)
 
     except NoSuchElementException:
         print('바텀 시트 미노출')
